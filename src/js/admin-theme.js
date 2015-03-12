01f67b91e59a6633f9784af80814ae317132e472
Josh Justice <josh@need-bee.com>
2015-03-12 13:35:45 -0400

    don't close a menu if an item is pre-selected

--- src/js/admin-theme.js
+++ src/js/admin-theme.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.side-nav li:has("ul")').children('ul').hide(); //hide submenu
+  $('.side-nav li:has("ul")').children('ul:not(:has(li.active))').hide(); //hide submenu
   $('.side-nav li:has("ul")').addClass('hasChildren'); // add class to li ul child
   $('.side-nav li:has("ul")').click(function(){
     $(this).toggleClass( "active" ); // add active class to clicked menu item
