02d5d67a025808b38907e0eb5a8d15ea2ae58a3e
Josh Justice <josh@need-bee.com>
2015-03-12 15:15:21 -0400

    prevents clicking a sidebar parent item from scrolling the page

--- src/js/admin-theme.js
+++ src/js/admin-theme.js
@@ -1,7 +1,8 @@
 $(function() {
   $('.side-nav li:has("ul")').children('ul:not(:has(li.active))').hide(); //hide submenu
   $('.side-nav li:has("ul")').addClass('hasChildren'); // add class to li ul child
-  $('.side-nav li:has("ul")').click(function(){
+  $('.side-nav li:has("ul")').click(function(evt){
+    evt.preventDefault();
     $(this).toggleClass( "active" ); // add active class to clicked menu item
     $(this).children('ul').slideToggle(); //toggle submenu
   });
