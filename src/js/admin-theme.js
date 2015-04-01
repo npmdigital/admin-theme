44cd13d35670d07d3a8ae5c3f09975a5a8c4e068
Josh Justice <josh@need-bee.com>
2015-04-01 14:42:39 -0400

    fixes bug where sub menu items not clickable

--- src/js/admin-theme.js
+++ src/js/admin-theme.js
@@ -1,9 +1,9 @@
 $(function() {
   $('.side-nav li:has("ul")').children('ul:not(:has(li.active))').hide(); //hide submenu
   $('.side-nav li:has("ul")').addClass('hasChildren'); // add class to li ul child
-  $('.side-nav li:has("ul")').click(function(evt){
+  $('.side-nav li:has("ul") > a').click(function(evt){
     evt.preventDefault();
-    $(this).toggleClass( "active" ); // add active class to clicked menu item
-    $(this).children('ul').slideToggle(); //toggle submenu
+    $(this).closest('li').toggleClass( "active" ); // add active class to clicked menu item
+    $(this).closest('li').children('ul').slideToggle(); //toggle submenu
   });
 });
